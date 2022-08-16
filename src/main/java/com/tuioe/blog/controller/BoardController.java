9ed2589c8deaa9ab569b36ec0fa31822a784d207
tuioe5679 <qodlgks123@naver.com>
2022-08-16 22:35:59 +0900

    feat:BoardController 클래스 생성

---
+++ src/main/java/com/tuioe/blog/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.tuioe.blog.controller;
+
+import com.tuioe.blog.Entity.Board;
+import com.tuioe.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @RequestMapping(value = "/boards",method = RequestMethod.GET)
+    public ResponseEntity list(){
+        List<Board> board = boardService.findAllBoard();
+        return new ResponseEntity(board, HttpStatus.OK);
+    }
+}
