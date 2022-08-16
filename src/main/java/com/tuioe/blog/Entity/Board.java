46690f0638980ed5b5ba5369d55347d2a8c2069a
tuioe5679 <qodlgks123@naver.com>
2022-08-16 22:24:47 +0900

    refactor: @Table 추가

--- src/main/java/com/tuioe/blog/Entity/Board.java
+++ src/main/java/com/tuioe/blog/Entity/Board.java
@@ -1,10 +1,10 @@
 package com.tuioe.blog.Entity;
 
 import lombok.Data;
-
 import javax.persistence.*;
 import java.util.Date;
 
+@Table(name = "board") //테이블 이름 지정 
 @Entity //Entity 클래스로 지정 (데이터베이스와 매핑)
 @Data //lombok의 어노테이션 (get,set,toString 메소드를 자동 생성)
 public class Board {
