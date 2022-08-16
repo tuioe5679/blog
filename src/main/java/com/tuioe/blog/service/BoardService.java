f27387a91abfd842af15177c71997433b39ede4c
tuioe5679 <qodlgks123@naver.com>
2022-08-16 22:27:19 +0900

    feat: BoardService 클래스 생성

---
+++ src/main/java/com/tuioe/blog/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.tuioe.blog.service;
+
+import com.tuioe.blog.Entity.Board;
+import com.tuioe.blog.repositroy.BoardRepositroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepositroy boardRepositroy;
+
+    public List<Board> findAllBoard(){
+        return boardRepositroy.findAll();
+    }
+}
