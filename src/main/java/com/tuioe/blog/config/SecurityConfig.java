d811ccb8043caa020a39997ca9cc1309ef513a97
tuioe5679 <qodlgks123@naver.com>
2022-08-21 23:19:24 +0900

    feat: 시큐리티 클래스 구성

---
+++ src/main/java/com/tuioe/blog/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.tuioe.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf()
+        .disable()
+
+        .formLogin()
+                .loginPage("/login");
+        return http.build();
+    }
+}
