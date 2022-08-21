dacbc1bcbeed4e3e68d3cfed77290346d26dd0af
tuioe5679 <qodlgks123@naver.com>
2022-08-21 23:57:12 +0900

    fix: mainConroller 오류 해결

---
+++ src/main/java/com/tuioe/blog/config/MvConfiguration.java
@@ -0,0 +1,15 @@
+package com.tuioe.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration// 스프링의 설정을 담당하는 어노테이션
+public class MvConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**") // "/"으로 시작하는 모든 요청을 처리
+                .addResourceLocations("classpath:/templates/");// 해당 요청을 처리할 파일의 경로를 지정
+    }
+}
