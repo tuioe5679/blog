9d3cfd9b98405a87552f1e9d3ccded9623d03f59
tuioe <qodlgks123@naver.com>
2022-08-23 15:15:39 +0900

    refactor: member 테이블 수정

--- src/main/java/com/tuioe/blog/Entity/member.java
+++ src/main/java/com/tuioe/blog/Entity/member.java
@@ -1,19 +1,25 @@
 package com.tuioe.blog.Entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Table(name = "member")
 @Entity
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Integer idx;
 
-    @Column(nullable = false,length = 15)
-    private String id;
+    @Column(nullable = false,length = 30)
+    private String email;
 
     @Column(nullable = false,length = 300)
     private String password;
@@ -27,6 +33,10 @@ public class Member {
     @Column(nullable = false,length = 15)
     private String phoneNumber;
 
-    @Column(nullable = false,length = 30)
-    private String email;
+    @Column(nullable = false,length = 10)
+    private String role;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createDate;
 }
