1be08e71cae4d112a4dfe3442e39add8d64e818b
tuioe5679 <qodlgks123@naver.com>
2022-08-23 22:28:48 +0900

    Entity 클래스 주석 추가

--- src/main/java/com/tuioe/blog/Entity/Comment.java
+++ src/main/java/com/tuioe/blog/Entity/Comment.java
@@ -6,23 +6,24 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Table(name = "comment")
-@Entity
-@Data
-@EntityListeners(AuditingEntityListener.class)
+@Table(name = "comment") // 테이블 이름 지정
+@Entity // Entity 클래스로 지정 (데이터베이스와 매핑)
+@Data // lombok의 어노테이션 (get,set,toString 메소드를 자동 생성)
+@EntityListeners(AuditingEntityListener.class)// 엔티티 리스너를 적용 (Auditing 기능을 포함시킨다) @CreateDate 기능 사용시 필요
 public class Comment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id// PK 기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// 기본키 생성 전략을 DB에게 위임(MySQL = Auto INCREMENT)
     private Integer id;
 
-    @Column(nullable = false,length = 30)
-    private String name;
+    @ManyToOne// 연관관계를 지정 n:1 관계를 설정한다
+    @JoinColumn(name="nickname")// 외래키를 지정 FK member의 PK를 외래키로 지정
+    private Member member;
 
-    @Column(nullable = false,length = 1000)
+    @Column(nullable = false,length = 1000)// null X 길이는 1000
     private String content;
 
-    @Column(nullable = false)
+    @Column(nullable = false)// null X
     @CreatedDate
     private LocalDateTime date;
 
--- src/main/java/com/tuioe/blog/Entity/Member.java
+++ src/main/java/com/tuioe/blog/Entity/Member.java
@@ -6,35 +6,35 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Table(name = "member")
-@Entity
-@Data
-@EntityListeners(AuditingEntityListener.class)
+@Table(name = "member") // 테이블 이름 지정
+@Entity // Entity 클래스로 지정 (데이터베이스와 매핑)
+@Data // lombok의 어노테이션 (get,set,toString 메소드를 자동 생성)
+@EntityListeners(AuditingEntityListener.class)// 엔티티 리스너를 적용 (Auditing 기능을 포함시킨다) @CreateDate 기능 사용시 필요
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Id// PK 기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// 기본키 생성 전략을 DB에게 위임(MySQL = Auto INCREMENT)
+    @Column(name = "member_id")// 이름 지정
     private Integer idx;
 
-    @Column(nullable = false,length = 30)
+    @Column(nullable = false,length = 30)// null X 길이는 30
     private String email;
 
-    @Column(nullable = false,length = 300)
+    @Column(nullable = false,length = 300)// null X 길이는 300 (BCryptPasswordEncoder 암호화)
     private String password;
 
-    @Column(nullable = false,length = 30)
+    @Column(nullable = false,length = 30)// null X 길이는 30
     private String name;
 
-    @Column(nullable = false,length = 30)
+    @Column(nullable = false,length = 30)// null X 길이는 30
     private String nickname;
 
-    @Column(nullable = false,length = 15)
+    @Column(nullable = false,length = 15)// null X 길이는 15
     private String phoneNumber;
 
-    @Column(nullable = false,length = 10)
+    @Column(nullable = false,length = 10)// null X 길이는 10
     private String role;
 
-    @Column(nullable = false)
-    @CreatedDate
+    @Column(nullable = false)// null X
+    @CreatedDate// Entity가 생성되어 저장할때 현재 시간을 자동 생성한다
     private LocalDateTime createDate;
 }
