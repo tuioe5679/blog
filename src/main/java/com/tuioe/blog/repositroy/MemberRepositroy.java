806312f2191641c1af0a3abe5ead619727bdaade
tuioe5679 <qodlgks123@naver.com>
2022-08-23 22:11:45 +0900

    feat: MemberRespositry JPA Query Method

--- src/main/java/com/tuioe/blog/repositroy/MemberRepositroy.java
+++ src/main/java/com/tuioe/blog/repositroy/MemberRepositroy.java
@@ -2,6 +2,13 @@ package com.tuioe.blog.repositroy;
 
 import com.tuioe.blog.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepositroy extends JpaRepository<Member, Integer> {
+
+    // 인터페이스에서 함수를 선언하고 필요한곳 에서 Override 하여 사용
+    // Select * from member where email = ?;
+    // Select 쿼리를 통해서 email을 가지는 유저 정보를 모두 가져옴 (따로 쿼리 작성 필요 X)
+
+     Member findByEmail(@Param("email") String email);
 }
