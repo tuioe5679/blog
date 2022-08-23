bef8227ed8abb63a96f6e745c2554cc99d0987e2
tuioe <qodlgks123@naver.com>
2022-08-23 15:20:24 +0900

    Spring Security 설정 클래스

--- src/main/java/com/tuioe/blog/config/SecurityConfig.java
+++ src/main/java/com/tuioe/blog/config/SecurityConfig.java
@@ -4,19 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean// 해당 메소드의 리턴되는 오브젝트를 IOC로 등록
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf()
-        .disable()
-
+        http.csrf().disable().
+        authorizeHttpRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
         .formLogin()
-                .loginPage("/login");
+                .loginPage("/login/form");
         return http.build();
     }
 }
