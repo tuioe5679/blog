319facd674face449c69c5fc6902efd24b16af10
tuioe5679 <qodlgks123@naver.com>
2022-08-29 00:23:18 +0900

    feat: 댓글 로직 구현

--- src/main/java/com/tuioe/blog/controller/CommentController.java
+++ src/main/java/com/tuioe/blog/controller/CommentController.java
@@ -1,7 +1,34 @@
 package com.tuioe.blog.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.tuioe.blog.dto.CommentDTO;
+import com.tuioe.blog.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @RequestMapping(value = "/comments",method = RequestMethod.GET)
+    public ResponseEntity list(Principal principal){
+        System.out.println(principal.getName());
+        return new ResponseEntity(commentService.findAllComment(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/comment/{id}",method = RequestMethod.GET)
+    public ResponseEntity findComment(@PathVariable int id){
+        return new ResponseEntity(commentService.findComment(id),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public ResponseEntity commentAdd(@RequestBody CommentDTO dto){
+        commentService.createComment(dto);
+        return new ResponseEntity("Create",HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/tuioe/blog/service/CommentService.java
+++ src/main/java/com/tuioe/blog/service/CommentService.java
@@ -1,12 +1,46 @@
 package com.tuioe.blog.service;
 
+import com.tuioe.blog.Entity.Comment;
+import com.tuioe.blog.Entity.Member;
+import com.tuioe.blog.dto.CommentDTO;
 import com.tuioe.blog.repositroy.CommentRepositroy;
+import com.tuioe.blog.repositroy.MemberRepositroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CommentService {
 
+    String username;
     @Autowired
     private CommentRepositroy commentRepositroy;
+
+    @Autowired
+    private MemberRepositroy memberRepositroy;
+
+    public List<CommentDTO> findAllComment(){
+        List<Comment> comments = commentRepositroy.findAll();
+        List<CommentDTO> responseDTO = new ArrayList<>();
+        for(Comment comment: comments){
+            responseDTO.add(CommentDTO.create(comment));
+        }
+        return responseDTO;
+    }
+
+    public void getUsername(String username){
+        this.username = username;
+    }
+
+    public CommentDTO findComment(int id){
+        return CommentDTO.create(commentRepositroy.findById(id).get());
+    }
+
+    public void createComment(CommentDTO dto){
+        Member member = memberRepositroy.findByEmail(username);
+        Comment comment = dto.commentCreate(dto);
+        comment.setMember(member);
+        commentRepositroy.save(comment);
+    }
 }
