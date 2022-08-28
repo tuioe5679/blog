54ed563451861ca993eaa25220bbb0189b475e0f
tuioe5679 <qodlgks123@naver.com>
2022-08-28 22:31:56 +0900

    feat: Member Validation(데이터 유효성 검사) 적용

--- src/main/java/com/tuioe/blog/Entity/Member.java
+++ src/main/java/com/tuioe/blog/Entity/Member.java
@@ -4,16 +4,15 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @Table(name = "member") // 테이블 이름 지정
 @Entity // Entity 클래스로 지정 (데이터베이스와 매핑)
 @Getter // Get 메소드 생성
-@Builder // Builder 패턴
 @NoArgsConstructor // 기본 생성자
-@AllArgsConstructor // 모든 인자를 받는 생성자 (Builder를 쓸때 NoArgsConstructor만 있으면 Builder 어노테이션 사용 불가 )
 @EntityListeners(AuditingEntityListener.class)// 엔티티 리스너를 적용 (Auditing 기능을 포함시킨다) @CreateDate 기능 사용시 필요
-
 public class Member {
     @Id// PK 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY)// 기본키 생성 전략을 DB에게 위임(MySQL = Auto INCREMENT)
@@ -23,20 +22,36 @@ public class Member {
     @Column(nullable = false,length = 30,unique = true)// null X 길이는 30 unique 제약조건 (테이블내의 유일한 값으로 갖는다) 중복 허용 안함
     private String email;
 
+
     @Column(nullable = false,length = 300)// null X 길이는 300 (BCryptPasswordEncoder 암호화)
     private String password;
+
 
     @Column(nullable = false,length = 30)// null X 길이는 30
     private String name;
 
+
     @Column(nullable = false,length = 30,unique = true)// null X 길이는 30 unique 제약조건 (테이블내의 유일한 값으로 갖는다) 중복 허용 안함
     private String nickname;
+
 
     @Column(nullable = false,length = 15)// null X 길이는 15
     private String phoneNumber;
 
     @Column(nullable = false,length = 10)// null X 길이는 10
     private String role;
+
+    @Builder
+    public Member(Integer idx, String email, String password, String name, String nickname, String phoneNumber, String role, LocalDateTime createDate) {
+        this.idx = idx;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.createDate = createDate;
+    }
 
     @Column(nullable = false)// null X
     @CreatedDate// Entity가 생성되어 저장할때 현재 시간을 자동 생성한다
--- src/main/java/com/tuioe/blog/dto/MemberDTO.java
+++ src/main/java/com/tuioe/blog/dto/MemberDTO.java
@@ -4,16 +4,30 @@ import com.tuioe.blog.Entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDTO {
 
+    @Email(message = "이메일 형식으로 입력해주세요")
+    @NotEmpty(message = "이메일는 필수 입력 입니다")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 입니다")
+    @Length(min = 6,max = 12, message = "비밀번호는 6자 이상, 12자 이하로 입력해주세요")
     private String password;
+
+    @NotEmpty(message = "이름는 필수 항목 입니다")
     private String name;
+
+    @NotEmpty(message = "닉네임는 필수 항목 입니다")
     private String nickname;
+
+    @NotEmpty(message = "핸드폰 번호는 필수 항목 입니다")
     private String phoneNumber;
 
     public static Member memberCreate(MemberDTO dto){
