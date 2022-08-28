305b66a17374cfd5e199c10de4cd8dbadaeb0c4c
tuioe5679 <qodlgks123@naver.com>
2022-08-28 21:50:29 +0900

    feat: CommentDTO 추가

---
+++ src/main/java/com/tuioe/blog/dto/CommentDTO.java
@@ -0,0 +1,30 @@
+package com.tuioe.blog.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.tuioe.blog.Entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+public class CommentDTO {
+    private String content;
+    private String nickname;
+    private LocalDateTime date;
+
+    public static CommentDTO create(Comment comment){
+        return new CommentDTO(comment.getContent(),
+                              null,
+                              comment.getDate());
+    }
+
+    public Comment commentCreate(CommentDTO commentDTO){
+        return Comment.builder()
+                .content(commentDTO.getContent())
+                .build();
+    }
+}
